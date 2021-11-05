c0006c929f595040ff06e1ee29995a26f2e3a8ee
Joshua Frasure <joshfrasure2@gmail.com>
2021-11-04 23:44:37 -0400

    updated code

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 'use strict';
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let storeFormEl = document.getElementById('addStore');
 
 function genRange(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 } 
 
-function Store(name, min, max, avgCookie, salesTotal, grandTotal){
+function Store(name, min, max, avgCookie){
     this.name = name;
     this.minCust = min;
     this.maxCust = max;
     this.avgCookie = avgCookie; 
-    this.salesTotal = salesTotal; 
-    this.grandTotal = grandTotal; 
+    this.salesTotal = []; 
+    this.grandTotal = 0; 
     
     Store.prototype.hourlyCust = function () {
         for(let i = 0; i < hours.length; i += 1){
             this.numOfCust = genRange(this.minCust, this.maxCust);
             this.custSales = Math.round(this.avgCookie * this.numOfCust);
-            this.salesTotal.push (this.custSales);
+            this.salesTotal.push(this.custSales);
             this.grandTotal = this.grandTotal + this.custSales;
             
         }
@@ -27,49 +28,13 @@ function Store(name, min, max, avgCookie, salesTotal, grandTotal){
 }
 let seattle = new Store("seattle", 23, 65, 6.3, [], 0)
 
-// let s1 = document.getElementById('s1')
-// for(let i = 0; i < seattle.salesTotal.length; i += 1){
-//     let salesLI = document.createElement("li")
-//     salesLI.textContent = seattle.salesTotal[i]
-//     // s1.appendChild(salesLI)
-    
-// }
-
 let tokyo = new Store("tokyo", 3, 24, 1.2, [], 0)
 
-// let s2 = document.getElementById('s2')
-// for(let i = 0; i < tokyo.salesTotal.length; i += 1){
-//     let salesLI = document.createElement("li")
-//     salesLI.textContent = tokyo.salesTotal[i]
-//     s2.appendChild(salesLI)
-    
-// }
 let dubai = new Store("dubai", 11, 38, 3.7, [], 0)
 
-// let s3 = document.getElementById('s3')
-// for(let i = 0; i < dubai.salesTotal.length; i += 1){
-//     let salesLI = document.createElement("li")
-//     salesLI.textContent = dubai.salesTotal[i]
-//     s3.appendChild(salesLI)
-// }
 let paris = new Store("paris", 20, 38, 2.3, [], 0)
 
-// let s4 = document.getElementById('s4')
-// for(let i = 0; i < paris.salesTotal.length; i += 1){
-//     let salesLI = document.createElement("li")
-//     salesLI.textContent = paris.salesTotal[i]
-//     s4.appendChild(salesLI)
-    
-// }
 let lima = new Store("lima", 2, 16, 4.6, [], 0)
-
-// let s5 = document.getElementById('s5')
-// for(let i = 0; i < lima.salesTotal.length; i += 1){
-//     let salesLI = document.createElement("li")
-//     salesLI.textContent = lima.salesTotal[i]
-//     s5.appendChild(salesLI)
-    
-// }
 
 const numberContainerElem = document.getElementById('container');
 
@@ -94,11 +59,10 @@ headerRowElem.appendChild(total);
 }
 tableHeader()
   Store.prototype.render = function(){
-    // this.hourlyCustomers()
-    // let body = document.getElementById('tbody')
+
     let row = document.createElement('tr')
     row.textContent = this.name;
-    salesTable.appendChild(row)    // trying this out!!
+    salesTable.appendChild(row)    
   for (let i = 0; i < hours.length; i++){
     let salesTotal = document.createElement('td')
     salesTotal.textContent = this.salesTotal[i]
@@ -113,3 +77,19 @@ tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
+
+function newStore (formSubmission) {
+  formSubmission.preventDefault();
+  let name = formSubmission.target.storeName.value;
+  let minimumCust = formSubmission.target.minCust.value;
+  let maximumCust = formSubmission.target.maxCust.value;
+  let avg = formSubmission.target.avgCookie.value;
+  let newMinCust = parseInt(minimumCust);
+  let newMaxCust = parseInt(maximumCust);
+  let newAvgDaily = parseInt(avg);
+  let newDataStore = new Store(name, newMinCust, newMaxCust, newAvgDaily);
+
+  newDataStore.render();
+  // tableFooter()
+}
+storeFormEl.addEventListener('submit', newStore);
